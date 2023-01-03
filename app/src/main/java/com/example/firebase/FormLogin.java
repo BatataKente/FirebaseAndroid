f0360b3d7992802036bbc2dfda7b76cbd822b527
Audioteca Ancap <danielpotoco@gmail.com>
2023-01-03 09:42:11 -0300

    added cadastro Form

--- app/src/main/java/com/example/firebase/FormLogin.java
+++ app/src/main/java/com/example/firebase/FormLogin.java
@@ -2,13 +2,33 @@ package com.example.firebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class FormLogin extends AppCompatActivity {
 
-    @Override protected void onCreate(Bundle savedInstanceState) {
+    private TextView text_tela_cadastro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_login);
-        getSupportActionBar().hide();
+        initializeComponents();
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(
+                        new Intent(
+                                FormLogin.this, FormCadastro.class
+                        )
+                    );
+                }
+            }
+        );
+    }
+    private void initializeComponents() {
+        text_tela_cadastro = findViewById(R.id.text_cadastrar);
     }
 }
