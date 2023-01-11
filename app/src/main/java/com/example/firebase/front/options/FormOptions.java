8f5028a3f7d8d792a09d23767f1f40c023c1741c
Audioteca Ancap <danielpotoco@gmail.com>
2023-01-11 16:01:19 -0300

    Added an menu, with scroll and linearLayout programatically

--- app/src/main/java/com/example/firebase/front/options/FormOptions.java
+++ app/src/main/java/com/example/firebase/front/options/FormOptions.java
@@ -1,10 +1,13 @@
 package com.example.firebase.front.options;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.firebase.front.home.FormHome;
+import com.example.firebase.front.register.FormCadastro;
 import com.example.firebase.utils.Initialization;
 
 public class FormOptions extends AppCompatActivity implements Initialization {
@@ -17,5 +20,13 @@ public class FormOptions extends AppCompatActivity implements Initialization {
     }
     @Override public void initialize() {
         view = new FormOptionsScrollView(this);
+        view.layout.addView(createButton(new FormCadastro()));
+        view.layout.addView(createButton(new FormHome()));
+    }
+    private Button createButton(AppCompatActivity activity) {
+        Button button = new Button(this);
+        String[] name = activity.getClass().getName().split("\\.");
+        button.setText(name[name.length-1]);
+        return button;
     }
 }
--- app/src/main/java/com/example/firebase/front/options/FormOptionsScrollView.java
+++ app/src/main/java/com/example/firebase/front/options/FormOptionsScrollView.java
@@ -1,18 +1,14 @@
 package com.example.firebase.front.options;
 
 import android.content.Context;
-import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
 
 import com.example.firebase.R;
-import com.example.firebase.front.home.FormHome;
-import com.example.firebase.front.register.FormCadastro;
 import com.example.firebase.utils.Initialization;
 
-public class FormOptionsScrollView extends ScrollView implements Initialization {
+public class FormOptionsScrollView extends NestedScrollView implements Initialization {
     LinearLayout layout;
     public FormOptionsScrollView(Context context) {
         super(context);
@@ -21,18 +17,8 @@ public class FormOptionsScrollView extends ScrollView implements Initialization 
         setBackgroundResource(R.drawable.background);
         addView(layout);
     }
-    @Override public void initialize() {
-        layout = new LinearLayout(getContext());
-    }
+    @Override public void initialize() {layout = new LinearLayout(getContext());}
     private void setLayout() {
         layout.setOrientation(LinearLayout.VERTICAL);
-        layout.addView(createButton(new FormCadastro()));
-        layout.addView(createButton(new FormHome()));
-    }
-    private Button createButton(AppCompatActivity activity) {
-        Button button = new Button(getContext());
-        String[] name = activity.getClass().getName().split("\\.");
-        button.setText(name[name.length-1]);
-        return button;
     }
 }
